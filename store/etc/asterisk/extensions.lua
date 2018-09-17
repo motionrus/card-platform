64c04f50286591575fa9198c0643f9b8c690f714
tytin@narod.ru <tytin@narod.ru>
2018-09-17 18:42:17 +0300

    lua script that reads the password from app.read
    * if the password is the same, the ANI number is returned, otherwise the error

--- store/etc/asterisk/extensions.lua
+++ store/etc/asterisk/extensions.lua
@@ -1,46 +1,46 @@
 
-local dial = function (context, extension)
-    app.noop("context: " .. context .. ", extension: " .. extension);
-    app.dial('SIP/' .. extension, 10);
-    
-    local dialstatus = channel["DIALSTATUS"]:get();
-    app.noop('dialstatus: '..dialstatus);
-    app.set("CHANNEL(language)=ru");
 
-    if dialstatus == 'BUSY' then
-        app.playback("followme/sorry");        
-    elseif dialstatus == 'CHANUNAVAIL' then 
-        app.playback("followme/sorry");
+
+function number(password)
+    -- return number if exist
+    numbers = {
+        ["803297"] = "501000",
+        ["331884"] = "501001",
+        ["862003"] = "501002",
+        ["980248"] = "501003",
+        ["572251"] = "501004",
+        ["609433"] = "501005",
+        ["274887"] = "501006",
+        ["557042"] = "501007",
+        ["915501"] = "501008",
+        ["518368"] = "501009"
+    };
+    if numbers[password] then
+        return numbers[password]
     end;
-
-    app.hangup();
 end;
 
-local ivr = function (context, extension)        
-    app.read("IVR_CHOOSE", "/var/menu/demo", 1, nil, 2, 3);
-    local choose = channel["IVR_CHOOSE"]:get();
 
-    if choose == '1' then
-        app.queue('1234');
-    elseif choose == '2' then
-        dial('internal', '101');
+local read_digits = function (context, extension)
+    app.read("DIGITS","ru/vm-reenterpassword")
+    local password = channel["DIGITS"]:get();
+    local check = number(password);
+    if check then
+        app.NoOp('correct password')
+        app.NoOp("context: " .. context .. ", extension: " .. extension);
+        app.Set('CALLERID(num)=' .. check);
+        app.dial('SIP/rtu/' .. extension);
     else
+        app.Noop('not correct password')
+        app.playback("ru/followme/sorry")
         app.hangup();
     end;
 end;
 
 extensions = {
-    ["internal"] = {
+    ["cardplatform"] = {
 
-        ["*12"] = function ()
-            app.sayunixtime();
-        end;
-
-        ["_1XX"] = dial;
-
-        ["200"] = ivr;
-
-        ["_NXXXXXX"] = outgoing_route_function;
+        ["_8X."] = read_digits;
     };
 };
 
