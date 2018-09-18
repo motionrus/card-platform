c0a5ec4a08ae755ce49aac4dede6303b8586df0a
tytin@narod.ru <tytin@narod.ru>
2018-09-18 13:17:09 +0300

    create new method check login and password
    * first I ask for my login and password. If the username and password are correct, then you can call

--- store/etc/asterisk/extensions.lua
+++ store/etc/asterisk/extensions.lua
@@ -20,8 +20,7 @@ function number(password)
     end;
 end;
 
-
-local read_digits = function (context, extension)
+local read_password = function (context, extension)
     app.read("DIGITS","ru/vm-reenterpassword")
     local password = channel["DIGITS"]:get();
     local check = number(password);
@@ -37,10 +36,50 @@ local read_digits = function (context, extension)
     end;
 end;
 
+function check_login_password(login, password)
+    -- body
+    numbers = {
+        ["501000"] = "803297",
+        ["501001"] = "331884",
+        ["501002"] = "862003",
+        ["501003"] = "980248",
+        ["501004"] = "572251",
+        ["501005"] = "609433",
+        ["501006"] = "274887",
+        ["501007"] = "557042",
+        ["501008"] = "915501",
+        ["501009"] = "518368",
+    };
+    if (numbers[login] == password) then
+        return true
+    end;
+end;
+
+local read_number_password = function (context, extension)
+    app.read("login","/var/menu/vm-enter-num-to-call")
+    local login = channel["login"]:get();
+    app.read("password","ru/vm-reenterpassword")
+    local password = channel["password"]:get();
+    local check = check_login_password(login, password)
+    if check then
+        app.NoOp('correct login/password')
+        app.NoOp("context: " .. context .. ", extension: " .. extension);
+        app.Set('CALLERID(num)=' .. login);
+        app.dial('SIP/rtu/' .. extension);
+    else
+        app.NoOp("not correct login or password")
+        app.playback("ru/followme/sorry")
+        app.hangup();
+    end;
+    -- body
+end;
+
+
 extensions = {
     ["cardplatform"] = {
 
-        ["_8X."] = read_digits;
+        ["_8X."] = read_number_password;
+        ["_7X."] = read_number_password;
     };
 };
 
